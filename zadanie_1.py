c45c45b8df2d419df9bbc1393f9ae88f6db1182c
FilipKusiak <fkusiak8@gmail.com>
2025-11-08 16:44:00 +0100

    Zadanie Pierwsze z Listą zakupów.

---
+++ zadanie_1.py
@@ -0,0 +1,8 @@
+# Przykład listy
+
+lista_zakupów = ["jabłka", "banany", "chleb", "mleko", "jajka"]
+print(lista_zakupów(1))  # Wyświetli "banany"
+print(lista_zakupów[1])  # Wyświetli "banany"
+print(lista_zakupów[0])  # Wyświetli "jabłka"
+lista_zakupów.append("masło")  # Dodaje "masło" do listy
+print(lista_zakupów)  # Wyświetli zaktualizowaną listę zakupów
