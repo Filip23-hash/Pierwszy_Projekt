5f6edc205e31f5ba7f519423227e83e59ecb56e6
FilipKusiak <fkusiak8@gmail.com>
2025-11-15 15:51:49 +0100

    Age

--- zadanie_1.py
+++ zadanie_1.py
@@ -6,3 +6,4 @@ print(lista_zakupów[1])  # Wyświetli "banany"
 print(lista_zakupów[0])  # Wyświetli "jabłka"
 lista_zakupów.append("masło")  # Dodaje "masło" do listy
 print(lista_zakupów)  # Wyświetli zaktualizowaną listę zakupów
+        
